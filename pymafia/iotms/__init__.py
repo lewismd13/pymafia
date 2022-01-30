c418f110c5093cc54b7e816e5afcc6283dd39f65
MrFizzyBubbs <MrFizzyBubbs@gmail.com>
2022-01-30 15:15:22 -0600

    Add cartography.py

--- pymafia/iotms/__init__.py
+++ pymafia/iotms/__init__.py
@@ -3,3 +3,6 @@ from pymafia.iotms import barrel_god
 
 # 2018
 from pymafia.iotms import boxing_daycare
+
+# 2020
+from pymafia.iotms import cartography
---
+++ pymafia/iotms/cartography.py
@@ -0,0 +1,37 @@
+import re
+from pymafia import ash, get_property, have as _have, Macro, Skill
+
+passive = Skill("Comprehensive Cartography")
+skill = Skill("Map the Monsters")
+
+
+def have():
+    """Return true if the player has the Comprehensive Cartography skill."""
+    return _have(passive)
+
+
+def monsters_mapped():
+    """Return the number monsters that have been mapped today."""
+    return get_property("_monstersMapped", int)
+
+
+def map_monster(location, monster, macro=Macro()):
+    """Map to a monster in a location."""
+    if not have() or monsters_mapped() >= 3:
+        return False
+
+    if not get_property("mappingMonsters"):
+        ash.use_skill(skill)
+    ash.visit_url(location.url)
+    assert (
+        ash.handling_choice() and ash.last_choice() == 1435
+    ), "failed to encounter the Map the Monsters noncombat adventure"
+
+    url = f"choice.php?pwd=&whichchoice=1435&option=1&heyscriptswhatsupwinkwink={monster.id}"
+    m = re.search("<!-- MONSTERID: (\\d+) -->", ash.visit_url(url))
+    assert (
+        m and int(m.group(1)) == monster.id
+    ), f"failed to enter combat with monster: {monster!r}"
+
+    ash.run_combat(macro)
+    return True
