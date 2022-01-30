aa4b3df33285641f5710974ace11da20d0f863b7
MrFizzyBubbs <MrFizzyBubbs@gmail.com>
2022-01-30 17:16:58 -0600

    Add pantogram.py

--- pymafia/iotms/__init__.py
+++ pymafia/iotms/__init__.py
@@ -5,7 +5,7 @@ from pymafia.iotms import crimbo_shrub
 from pymafia.iotms import barrel_god, machine_elf
 
 # 2017
-from pymafia.iotms import love_tunnel, mumming_trunk
+from pymafia.iotms import love_tunnel, mumming_trunk, pantogram
 
 # 2018
 from pymafia.iotms import boxing_daycare, god_lobster
